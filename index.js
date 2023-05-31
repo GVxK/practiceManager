9cc4b3b9e4ce3981d53d0337a5b43fceac38eb61
GVxK <guilhermevxk@gmail.com>
2023-05-31 02:00:28 -0300

    a

--- index.js
+++ index.js
@@ -1,22 +1,72 @@
 const timerEl = document.getElementById("timer")
 const minutesEl = document.getElementById("minutes")
 const secondsEl = document.getElementById("seconds")
-
+const startBtn = document.getElementById("start-btn")
+const video1El = document.getElementById("video1")
+const video2El = document.getElementById("video2")
 
 let minutes = 15
 let seconds = 05
-let timer = `${minutes}:${seconds}`
+let minutesFormatted = minutes.toString().padStart(2, '0')
+let secondsFormatted = seconds.toString().padStart(2, '0')
+let workTime = false
+let breakTime = false
+let timerTimeout = ""
+
+
+  startBtn.addEventListener("click", handleButton)
+
+
+function handleButton() {
+  if (startBtn.textContent === "START") {
+    handlePomodoro()
+    startBtn.textContent = "PAUSE"
+  } else if (startBtn.textContent === "PAUSE") {
+    startBtn.textContent = "RESUME"
+    seconds++
+  } else if (startBtn.textContent === "RESUME") {
+    startBtn.textContent = "PAUSE"
+    updateTimer()
+  }
+}
+function handlePomodoro() {
+  handleWork()
+  // handleBreak()
+  
+}
+function handleWork() {
+  minutes = 0
+  seconds = 10
+  video1El.contentWindow.postMessage('{"event":"command","func":"playVideo","args":""}', '*')
+  updateTimer()
+}
+function handleBreak() {
+  minutes = 0
+  seconds = 5 
+  updateTimer()
+}
 
 function updateTimer() {
-  // updateSeconds()
-  // updateMinutes()
   seconds--
-  if (seconds <= 00) {
+  
+    // if (startBtn.textContent === "PAUSE") {
+    //   startBtn.disabled = false
+    //   startBtn.addEventListener("click", () => {
+    //     clearTimeout(timerTimeout)
+    //    })
+    // }
+  if (minutes >= 1 && seconds < 0) {
     minutes--
     seconds = 59
   }
-  minutesEl.textContent = minutes
-  secondsEl.textContent = seconds
-  setTimeout(updateTimer, 1000)
+  minutesEl.textContent = minutes.toString().padStart(2, '0')
+  secondsEl.textContent = seconds.toString().padStart(2, '0')
+  let timerTimeout = setTimeout(updateTimer, 1000)
+  if (minutes < 1 && seconds <= 0) {
+    clearTimeout(timerTimeout)
+    startBtn.textContent = "START"
+  }
+  if (startBtn.textContent === "RESUME") {
+    clearTimeout(timerTimeout)
+  } 
 }
-updateTimer()
